728c4187a22745093645252cb76aa2078fd3762e
Reilly Thompson <reillythompson@Reillys-MacBook-Air.local>
2023-05-20 15:13:28 -0700

    move artistTopTracks into separate child component

--- src/components/ArtistProfile/ArtistProfile.js
+++ src/components/ArtistProfile/ArtistProfile.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ArtistTopTracks from "./ArtistTopTracks";
 
 const ArtistProfile = ({ artistData, artistProfileInfo, artistTopTracks }) => {
   const [audio, setAudio] = useState(null);
@@ -11,23 +12,7 @@ const ArtistProfile = ({ artistData, artistProfileInfo, artistTopTracks }) => {
   for (const key of keys) {
     const value = artistData[key];
   }
-
-  const handlePlay = (previewUrl) => {
-    if (audio) {
-      audio.pause();
-    }
-
-    const newAudio = new Audio(previewUrl);
-    setAudio(newAudio);
-    newAudio.play();
-  };
-
-  const handlePause = () => {
-    if (audio) {
-      audio.pause();
-    }
-  };
-
+  
   return (
     <div>
       <h1>{artistData.name}</h1>
@@ -36,25 +21,8 @@ const ArtistProfile = ({ artistData, artistProfileInfo, artistTopTracks }) => {
       <h3>Genres: {artistData.genres.join(" , ")}</h3>
       <img src={artistData.images[1].url} />
       <h2>Top tracks</h2>
-      <ul>
-        {artistTopTracks &&
-          artistTopTracks.tracks.map((track) => (
-            <li key={track.id}>
-              {/* To do: Display button that plays preview of song */}
-              {/* <a href={track.preview_url}>{track.name}</a> */}
-              <h3>
-                <button 
-                onClick={() => handlePlay(track.preview_url)}
-                >
-                  {track.name}
-                </button>
-                <button onClick={handlePause}>Pause</button>
-              </h3>
-              <p>{track.album.name}</p>
-            </li>
-          ))}
-      </ul>
-    </div>
+      <ArtistTopTracks artistTopTracks={artistTopTracks} />
+      </div>
   );
 };
 
---
+++ src/components/ArtistProfile/ArtistTopTracks.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const ArtistTopTracks = ({ artistTopTracks }) => {
+  const [audio, setAudio] = useState(null);
+
+  const handlePlay = (previewUrl) => {
+    if (audio) {
+      audio.pause();
+    }
+
+    const newAudio = new Audio(previewUrl);
+    setAudio(newAudio);
+    newAudio.play();
+  };
+
+  const handlePause = () => {
+    if (audio) {
+      audio.pause();
+    }
+  };
+
+  return (
+      <ul>
+        {artistTopTracks &&
+          artistTopTracks.tracks.map((track) => (
+            <li key={track.id}>
+              {/* To do: Display button that plays preview of song */}
+              <h3>
+                <button 
+                onClick={() => handlePlay(track.preview_url)}
+                >
+                  {track.name}
+                </button>
+                <button onClick={handlePause}>Pause</button>
+              </h3>
+              
+            </li>
+          ))}
+      </ul>
+  );
+};
+
+export default ArtistTopTracks;
