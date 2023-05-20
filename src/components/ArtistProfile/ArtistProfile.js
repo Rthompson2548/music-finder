1a110fd75f38f5fa9a032505b5845629e3ddd54e
Reilly Thompson <reillythompson@Reillys-MacBook-Air.local>
2023-05-20 15:00:50 -0700

    add play/pause functionality and in-screen audio play for top-tracks preview_url

--- src/components/ArtistProfile/ArtistProfile.js
+++ src/components/ArtistProfile/ArtistProfile.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 const ArtistProfile = ({ artistData, artistProfileInfo, artistTopTracks }) => {
+  const [audio, setAudio] = useState(null);
+  
   if (!artistProfileInfo) {
     return <p>Loading artist details...</p>;
   }
@@ -10,24 +12,47 @@ const ArtistProfile = ({ artistData, artistProfileInfo, artistTopTracks }) => {
     const value = artistData[key];
   }
 
+  const handlePlay = (previewUrl) => {
+    if (audio) {
+      audio.pause();
+    }
+
+    const newAudio = new Audio(previewUrl);
+    setAudio(newAudio);
+    newAudio.play();
+  };
+
+  const handlePause = () => {
+    if (audio) {
+      audio.pause();
+    }
+  };
 
   return (
     <div>
       <h1>{artistData.name}</h1>
       <h2>#{artistData.popularity}</h2>
       <h2>Followers: {artistData.followers.total}</h2>
-      <h2>Genre: {artistData.genres.join(" , ")}</h2>
+      <h3>Genres: {artistData.genres.join(" , ")}</h3>
       <img src={artistData.images[1].url} />
+      <h2>Top tracks</h2>
       <ul>
-        {
-          artistTopTracks && artistTopTracks.tracks.map((track) =>(
+        {artistTopTracks &&
+          artistTopTracks.tracks.map((track) => (
             <li key={track.id}>
-              <div>{track.name}</div>
               {/* To do: Display button that plays preview of song */}
-              <div>{track.preview_url}</div>
+              {/* <a href={track.preview_url}>{track.name}</a> */}
+              <h3>
+                <button 
+                onClick={() => handlePlay(track.preview_url)}
+                >
+                  {track.name}
+                </button>
+                <button onClick={handlePause}>Pause</button>
+              </h3>
+              <p>{track.album.name}</p>
             </li>
-          ))
-        }
+          ))}
       </ul>
     </div>
   );
