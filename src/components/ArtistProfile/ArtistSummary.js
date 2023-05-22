af8a03845fc7ab125fd67e06f1535c60b4aa5558
Reilly Thompson <reillythompson@Reillys-MacBook-Air.local>
2023-05-22 11:05:51 -0700

    added css

--- src/components/ArtistProfile/ArtistSummary.js
+++ src/components/ArtistProfile/ArtistSummary.js
@@ -4,16 +4,20 @@ import "../ArtistProfile/ArtistProfile.css";
 
 const ArtistSummary = ({ artistData }) => {
   return (
-    <div>
-      <h2>#{artistData.popularity}</h2>
-      <h2>Followers: {artistData.followers.total}</h2>
-      <h3>Genres</h3>
-      <ul>
-        {artistData.genres.map((genre) => (
-          <li className="genre">{genre}</li>
-        ))}
-      </ul>
-      <img src={artistData.images[1].url} />
+    <div className="artist-summary">
+      <div>
+        <img src={artistData.images[2].url} />
+      </div>
+      <div className="artist-summary-details">
+        <h2>#{artistData.popularity}</h2>
+        <h3>{artistData.followers.total} followers</h3>
+        <h3>Genres</h3>
+        <ul>
+          {artistData.genres.map((genre) => (
+            <li className="genre">{genre}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
