db46af8cde18ad36819af10c49fbe273b8e174d3
Reilly Thompson <reillythompson@Reillys-MacBook-Air.local>
2023-05-22 10:59:19 -0700

    add play button for top tracks

--- src/components/ArtistProfile/ArtistTopTracks.js
+++ src/components/ArtistProfile/ArtistTopTracks.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../ArtistProfile/ArtistProfile.css";
 
 const ArtistTopTracks = ({ artistTopTracks, audio, setAudio }) => {
   const handlePlay = (previewUrl) => {
@@ -21,14 +22,23 @@ const ArtistTopTracks = ({ artistTopTracks, audio, setAudio }) => {
     <ul>
       {artistTopTracks &&
         artistTopTracks.tracks.map((track) => (
-          <li key={track.id}>
-            {/* To do: Display button that plays preview of song */}
-            <h3>
-              <button
+          <li key={track.id} className="top-track">
+            <div className="top-track_container">
+              <img src={track.album.images[2].url} />
+              {/* To do: Display button that plays preview of song */}
+              <h3
                 onClick={() => handlePlay(track.preview_url)}
                 disabled={track.preview_url === null}
               >
                 {track.name}
+              </h3>
+            </div>
+            <div>
+              <button
+                onClick={() => handlePlay(track.preview_url)}
+                disabled={track.preview_url === null}
+              >
+                Play
               </button>
               <button
                 onClick={handlePause}
@@ -36,8 +46,7 @@ const ArtistTopTracks = ({ artistTopTracks, audio, setAudio }) => {
               >
                 Pause
               </button>
-              <img src={track.album.images[2].url} />
-            </h3>
+            </div>
           </li>
         ))}
     </ul>
